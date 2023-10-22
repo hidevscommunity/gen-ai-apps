0490b68b9035dd94cb7214fef55b647242409766
hidevscommunity <hidevscommunity@gmail.com>
2023-10-22 14:11:53 +0530

    Update app.py

--- useful/SpeakIntro/app.py
+++ useful/SpeakIntro/app.py
@@ -1,18 +1,37 @@
+# Let's get pipelines from transformers
 from transformers import pipeline
+# Let's import Gradio
 import gradio as gr
 
 def main():
+  
+    # Let's set up the model
+    model = pipeline("automatic-speech-recognition", model="moraxgiga/audio_test")
+    title = "Audio2Text"
+    description = "Record your audio in English and send it in order to received a transcription"
 
-    model = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-large-xlsr-53-spanish")
 
+    # Function
     def transcribe(audio):
+        # Let's invoke "model" defined above
         text = model(audio)["text"]
         return text
 
-    gr.Interface(
+
+    # Interface Set-Up
+    '''gr.Interface(
         fn=transcribe,
         inputs=[gr.Audio(source="microphone", type="filepath")],
-        outputs=["textbox"],
+        title="Audio-to-text",
+        description="kat enterprises llc demo text-to-speech model",
+        outputs=["textbox"]
     ).launch()
+    '''
 
-
+    demo = gr.Interface(fn=transcribe ,
+                        inputs=[gr.Audio(source="microphone", type="filepath")],
+                        outputs=[gr.Textbox(label="Result", lines=3)],
+                        title="Audio-to-text",
+                        description="kat enterprises llc demo text-to-speech model"
+                    )
+    demo.launch()
