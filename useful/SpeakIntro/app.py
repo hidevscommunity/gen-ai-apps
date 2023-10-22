ec42a973505c2db3406f93b70500e943b5894c82
hidevscommunity <hidevscommunity@gmail.com>
2023-10-22 14:01:54 +0530

    Update app.py

--- useful/SpeakIntro/app.py
+++ useful/SpeakIntro/app.py
@@ -1,16 +1,18 @@
 from transformers import pipeline
 import gradio as gr
 
-model = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-large-xlsr-53-spanish")
+def main():
 
-def transcribe(audio):
-  text = model(audio)["text"]
-  return text
+    model = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-large-xlsr-53-spanish")
 
-gr.Interface(
-    fn=transcribe,
-    inputs=[gr.Audio(source="microphone", type="filepath")],
-    outputs=["textbox"],
-).launch()
+    def transcribe(audio):
+        text = model(audio)["text"]
+        return text
+
+    gr.Interface(
+        fn=transcribe,
+        inputs=[gr.Audio(source="microphone", type="filepath")],
+        outputs=["textbox"],
+    ).launch()
 
 
